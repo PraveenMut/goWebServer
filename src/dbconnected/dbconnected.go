ec415a5183eb96d31403ff081f39970a2d526ada
Praveen Muthu <praveen.mut@gmail.com>
2021-01-01 20:52:43 +1100

    Refactor and initialisation of db logic

--- src/dbconnected/dbconnected.go
+++ src/dbconnected/dbconnected.go
@@ -3,13 +3,24 @@ package main
 import (
 	"database/sql"
 	"log"
-	"go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-db, err := sql.Open("mysql", "root:root@data:3306/mydatabase")
+func main() {
+	// create connection to the MySQL database
+	db, err := sql.Open("mysql", "root:root@(localhost:3306)/mydatabase")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-err := db.Ping()
+	// initalise the first connection to the database by pinging it and checking for error
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-if err != nil {
-	log.Fatal(err)
+	quoteLengthQuery := `SELECT COUNT(id) FROM quotes`
+	if _, err := db.Exec(quoteLengthQuery); err != nil {
+		log.Fatal(err)
+	}
 }
