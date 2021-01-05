aaf27084c27e8c554cc6d5654d8278ff4a0e6ab3
Praveen Muthu <praveen.mut@gmail.com>
2021-01-05 14:01:00 +1100

    Revert "Refactor Routings by creating wrapper functions"
    This reverts commit 151a4ac15d698f732dcc824c4957a3a55eac9f7e.

--- src/fullserver/server.go
+++ src/fullserver/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"encoding/json"
 	"net/http"
@@ -10,15 +11,7 @@ import (
 
 )
 
-func initaliseDB() {
-	db, err := sql.Open("mysql", "root:root@(localhost:3306)/mydatabase")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
-}
-
-func setQuoteHandler(w, r, db) {
+func setQuote(w http.ResponseWriter, r *http.Request) {
 	quote := "You and me, we're meant to be, roaming free, in harmony. One fine day, we'll fly away. Don't you know that Rome wasn't built in a day."
 	result, err := db.Exec(`INSERT INTO quotes (quote) VALUES ?`, quote)
 	if err != nil {
@@ -30,7 +23,7 @@ func setQuoteHandler(w, r, db) {
 	json.NewEncoder(w).Encode(map[string]int{"result": idResult})
 }
 
-func getQuoteHandler(w, r, db) {
+func getQuote(w http.ResponseWriter, r *http.Request) {
 	// query a single user from the db
 	type quoteModel struct {
 		id int 
@@ -50,14 +43,14 @@ func getQuoteHandler(w, r, db) {
 }
 
 func main() {
-	db := initaliseDB()
+	db, err := sql.Open("mysql", "root:root@(localhost:3306)/mydatabase")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/get-quote", func(w http.ResponseWriter, r *http.Request) {
-		getQuoteHandler(w, r, db)
-	}).Methods("GET")
-	r.HandleFunc("/api/v1/set-quote", func(w. http.ResponseWriter, r *http.Request) {
-		setQuoteHandler(w, r, db)
-	}).Methods("POST")
+	r.HandleFunc("/api/v1/get-quote", getQuote).Methods("GET")
+	r.HandleFunc("/api/v1/set-quote", setQuote).Methods("POST")
 	r.HandleFunc("/api/v1/healthz", func(w http.ResponseWriter, r *http.Request) {
 		if err := db.Ping(); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
