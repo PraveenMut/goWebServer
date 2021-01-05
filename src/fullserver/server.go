abaa797e8178a2257ab20eac491e4710df7ffdbf
Praveen Muthu <praveen.mut@gmail.com>
2021-01-05 15:33:10 +1100

    Refactor the application to take a dbConnection as a struct
    The previous method of exposing the db connection to routing handlers still suffered from scoping issues.
    
    Because of that, it was best to reachitect the application in where the dbconnection is exposed through a globally defined struct, which then the handlers are wrapped around and defined inside of it.
    
     By creating these dbconnection methods with the handler methods exposed, allows for the HTTP HandleFunc method to take that method in, as the struct field db now points to the mySQL connection.

--- src/fullserver/server.go
+++ src/fullserver/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"encoding/json"
 	"net/http"
@@ -11,19 +10,34 @@ import (
 
 )
 
-func setQuote(w http.ResponseWriter, r *http.Request) {
+type DatabaseConnection struct {
+	db *sql.DB
+}
+
+func initaliseDB() *sql.DB {
+	databaseConnection, err := sql.Open("mysql", "root:root@(localhost:3306)/mydatabase")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return databaseConnection
+}
+
+func (dbConnection *DatabaseConnection) setQuote(w http.ResponseWriter, r *http.Request) {
 	quote := "You and me, we're meant to be, roaming free, in harmony. One fine day, we'll fly away. Don't you know that Rome wasn't built in a day."
-	result, err := db.Exec(`INSERT INTO quotes (quote) VALUES ?`, quote)
+	result, err := dbConnection.db.Exec(`INSERT INTO quotes (quote) VALUES ?`, quote)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(map[string]bool{"posted": false})
 	}
-	idResult = result.LastInsertID()
+	idResult, err := result.LastInsertId()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]int{"result": idResult})
+	json.NewEncoder(w).Encode(map[string]int64{"result": idResult})
 }
 
-func getQuote(w http.ResponseWriter, r *http.Request) {
+func (dbConnection *DatabaseConnection) getQuote(w http.ResponseWriter, r *http.Request) {
 	// query a single user from the db
 	type quoteModel struct {
 		id int 
@@ -34,7 +48,7 @@ func getQuote(w http.ResponseWriter, r *http.Request) {
 	singleQuoteQuery := "SELECT id, quote FROM quotes WHERE id = ?"
 	
 	// execute query by searching for the fields defined in struct
-	if err := db.QueryRow(singleQuoteQuery, 1).Scan(&q.id, &q.quote); err != nil {
+	if err := dbConnection.db.QueryRow(singleQuoteQuery, 1).Scan(&q.id, &q.quote); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(map[string]string{"quote": "unable to retrieve"})
 	}
@@ -43,16 +57,12 @@ func getQuote(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@(localhost:3306)/mydatabase")
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	dbConnection := &DatabaseConnection{db: initaliseDB()}
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/get-quote", getQuote).Methods("GET")
-	r.HandleFunc("/api/v1/set-quote", setQuote).Methods("POST")
+	r.HandleFunc("/api/v1/get-quote", dbConnection.getQuote).Methods("GET")
+	r.HandleFunc("/api/v1/set-quote", dbConnection.setQuote).Methods("POST")
 	r.HandleFunc("/api/v1/healthz", func(w http.ResponseWriter, r *http.Request) {
-		if err := db.Ping(); err != nil {
+		if err := dbConnection.db.Ping(); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(map[string]bool{"healthy": false})
 		} else {
